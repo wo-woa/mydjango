40e82c2df9a606f3bdc66a7b89c8bcbdb6fd80ee
31884 <318844856@qq.com>
2019-05-17 00:02:09 +0800

    use bootstrap ,widget_tweaks

--- boards/views.py
+++ boards/views.py
@@ -22,30 +22,39 @@ def board_topics(request, pk):
 
 
 def new_topic(request, pk):
-    # board = get_object_or_404(Board, pk=pk)
-    # user = User.objects.first()#  get the currently logged in user
-    # if request.method == 'POST':
-    #     form = NewTopicForm(request.POST)
-    #     if form.is_valid():
-    #         topic = form.save()
-    #         return redirect('board_topics', pk=board.pk)
-    # else:
-    #     form = NewTopicForm()
-    # return render(request, 'new_topic.html', {'form': form})
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get the currently logged in user
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-        user = User.objects.first()  # 临时使⽤⼀个账号作为登录⽤户
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-        return redirect('board_topics', pk=board.pk)  # redirect to the created topic page
-    return render(request, 'new_topic.html', {'board': board})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board,'form': form})
+
+    # board = get_object_or_404(Board, pk=pk)
+    # if request.method == 'POST':
+    #     subject = request.POST['subject']
+    #     message = request.POST['message']
+    #     user = User.objects.first()  # 临时使⽤⼀个账号作为登录⽤户
+    #     topic = Topic.objects.create(
+    #         subject=subject,
+    #         board=board,
+    #         starter=user
+    #     )
+    #     post = Post.objects.create(
+    #         message=message,
+    #         topic=topic,
+    #         created_by=user
+    #     )
+    #     return redirect('board_topics', pk=board.pk)  # redirect to the created topic page
+    # return render(request, 'new_topic.html', {'board': board})
